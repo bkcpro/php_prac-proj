26cba955f3f2ffd1897d2f4d0fcd9c210d48ed42
Bhargava <intuitivegrad30@gmail.com>
2018-03-16 18:55:42 -0700

    GET

---
+++ ex2.php
@@ -0,0 +1,19 @@
+<?php
+
+  echo "<h1>The username is: {$_GET['username']}</h1>";
+  echo "<h2>The lastname is: {$_GET['lastname']}</h2>";
+  echo "<h3>The firstname is: {$_GET['firstname']}</h3>";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
