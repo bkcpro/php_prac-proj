334969333e90175a9c4a289e95960bb77493144a
Bhargava <intuitivegrad30@gmail.com>
2018-03-26 16:40:48 -0700

    method chaining

---
+++ manipulate/text_manipulate.php
@@ -0,0 +1,27 @@
+<?php
+
+  class text{
+
+    public function __construct($text){
+
+      $this->text = $text;
+    }
+
+    public function print(){
+      echo $this->text;
+    }
+
+    public function toUpperCase(){
+      $this->text = strtoupper($this-text);
+
+      return $this;
+    }
+
+
+
+  }
+
+
+
+
+?>
---
+++ method_chaining.php
@@ -0,0 +1,22 @@
+<?php
+
+  require("manipulate/text_manipulate.php");
+
+  $myname = new Text("my name is Bhargava Kunchanapalli");
+
+  $myname->print();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
