180bfabd82552d775819da8bf21dd5df51cb45a4
Bhargava <intuitivegrad30@gmail.com>
2018-03-29 20:29:36 -0700

    includes

---
+++ simple_site/includes/header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+    <a href="/simple_site"> Home </a>
+    <a href="/simple_site/about.php"> About </a>
+    <a href="/simple_site/projects.php"> Projects </a>
+    <a href="/simple_site/contact.php"> Contacts </a>
+  </nav>
+</header>
--- simple_site/projects.php
+++ simple_site/projects.php
@@ -40,14 +40,7 @@
 
   <body>
     <div class = "container">
-      <header>
-        <nav>
-          <a href="/simple_site"> Home </a>
-          <a href="/simple_site/about.php"> About </a>
-          <a href="/simple_site/projects.php"> Projects </a>
-          <a href="/simple_site/contact.php"> Contacts </a>
-        </nav>
-      </header>
+      <?php include('includes/header.php') ?>
 
       <section>
 
