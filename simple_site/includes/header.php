c24f75b524c332f8a0eb91352ff135b060d950fd
Bhargava <intuitivegrad30@gmail.com>
2018-03-30 09:19:33 -0700

    active changes

--- simple_site/includes/header.php
+++ simple_site/includes/header.php
@@ -1,8 +1,8 @@
 <header>
   <nav>
-    <a href="/simple_site" class="<?php echo ($location == '/') ? 'active' : ''; ?> "> Home </a>
-    <a href="/simple_site/about.php" class="<?php echo($location == 'about.php') ? 'active' : ''; ?> "> About </a>
-    <a href="/simple_site/projects.php" class="<?php echo($location == 'projects.php') ? 'active' : ''; ?> "> Projects </a>
-    <a href="/simple_site/contact.php" class="<?php echo($location == 'contact.php') ? 'active' : ''; ?> "> Contacts </a>
+    <a href="/simple_site/index.php" class="<?php echo ($location == 'index.php') ? 'active' : ''; ?> "> Home </a>
+    <a href="/simple_site/about.php" class="<?php echo ($location == 'about.php') ? 'active' : ''; ?> "> About </a>
+    <a href="/simple_site/projects.php" class="<?php echo ($location == 'projects.php') ? 'active' : ''; ?> "> Projects </a>
+    <a href="/simple_site/contact.php" class="<?php echo ($location == 'contact.php') ? 'active' : ''; ?> "> Contacts </a>
   </nav>
 </header>
