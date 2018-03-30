33def3964c3eee4b51364a4671e3d74bff22f58a
Bhargava <intuitivegrad30@gmail.com>
2018-03-29 20:31:55 -0700

    added gallery.php

---
+++ simple_site/gallery.php
@@ -0,0 +1,52 @@
+//delete line: ctrl+shift+k
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> Testing simple php website</title>
+
+
+    <style>
+
+      body{
+        background: #1b1b1b;
+      }
+
+      .container{
+        margin: 0 auto;
+        max-width: 1200px;
+      }
+
+      header{
+        margin: 100px 0 0;
+        background: #000;
+        color: white;
+        text-align: center;
+      }
+
+      section{
+        background: #fff;
+        min-height: 600px;
+      }
+
+      header a{
+        color: white;
+        padding: 20px;
+        display: inline-block;
+      }
+    </style>
+
+  </head>
+
+  <body>
+    <div class = "container">
+      <?php include('includes/header.php') ?>
+
+      <section>
+
+        <h1> Gallery </h1>
+      </section>
+    </div>
+  </body>
+
+</html>
--- simple_site/includes/header.php
+++ simple_site/includes/header.php
@@ -3,6 +3,7 @@
     <a href="/simple_site"> Home </a>
     <a href="/simple_site/about.php"> About </a>
     <a href="/simple_site/projects.php"> Projects </a>
+    <a href="/simple_site/gallery.php"> Gallery </a>
     <a href="/simple_site/contact.php"> Contacts </a>
   </nav>
 </header>
