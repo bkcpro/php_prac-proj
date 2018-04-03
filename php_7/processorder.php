5a11b9dd3366dfe6143f9509d8de7231f832fa12
bkcpro <intuitivegrad30@gmail.com>
2018-04-03 12:34:24 -0700

    lamp start

---
+++ php_7/processorder.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> "Php script for Auto-shop "</title>
+  </head>
+
+  <body>
+    <h1> Bob's auto parts </h1>
+    <h2> Order results </h2>
+    <?php
+      echo "<h3> Order processed </h3>";
+    ?>
+  </body>
+</html>
