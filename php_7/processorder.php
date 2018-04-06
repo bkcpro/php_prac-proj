d5f5e4c40b717156c06fcb86fbecc8517b05c547
bkcpro <intutiivegrad30@gmail.com>
2018-04-06 16:12:27 -0700

    form totals

--- php_7/processorder.php
+++ php_7/processorder.php
@@ -2,9 +2,20 @@
 <html lang="en">
   <head>
     <title> "Php script for Auto-shop "</title>
+
+    <style>
+      .listOfItems{
+        color: blue;
+        background-color: black;
+        max-width: 100px;
+      }
+    </style>
+
   </head>
 
   <body>
+
+
     <h1> Bob's auto parts </h1>
     <h2> Order results </h2>
     <?php
@@ -13,11 +24,35 @@
       $oilqty = $_POST['oilqty'];
       $sparkqty = $_POST['sparkqty'];
 
+      echo "<div class = \"listOfItems\">
+        $tireqty      Tire(s) <br />
+        $oilqty       Oil bottle(s) <br />
+        $sparkqty     Spark plug(s) <br />
+      </div>";
 
-      echo htmlspecialchars($tireqty) . ' tires<br />';
-      echo htmlspecialchars($oilqty) . ' oil<br />';
-      echo htmlspecialchars($sparkqty) . ' spark plugs<br />';
 
+      $totalqty = 0;
+      $totalqty = $tireqty+$oilqty+$sparkqty;
+
+      // Fix emsp's, find alternative
+
+      echo "<p> Items ordered: &emsp;&emsp;&emsp;&emsp;&emsp;" . $totalqty . "<br />";
+      $totalamount = 0.00;
+
+      define("TIREPRICE", 50);
+      define("OILPRICE", 10);
+      define("SPARKPRICE", 4);
+
+      $totalamount = $tireqty * TIREPRICE
+                + $oilqty * OILPRICE
+                + $sparkqty * SPARKPRICE;
+
+      echo "Subtotal: &emsp;&emsp;&emsp;&emsp;&emsp;$" . number_format($totalamount, 2) . "<br />";
+
+      $taxrate = 0.10;
+      $totalamount = $totalamount + $taxrate * $totalamount;
+      echo "Total including tax: &emsp;&emsp;$" . number_format($totalamount) . "</p>";
     ?>
+
   </body>
 </html>
