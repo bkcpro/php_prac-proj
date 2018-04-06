999e7c81b8ab3e9fc053ea6aac4b9c830f9648af
bkcpro <intutiivegrad30@gmail.com>
2018-04-06 16:53:42 -0700

    changed UI and added discount

--- php_7/processorder.php
+++ php_7/processorder.php
@@ -7,8 +7,14 @@
       .listOfItems{
         color: blue;
         background-color: black;
-        max-width: 100px;
+        max-width: 140px;
       }
+
+      .iDiv{
+        color: red;
+        margin: 20px;
+      }
+
     </style>
 
   </head>
@@ -18,16 +24,37 @@
 
     <h1> Bob's auto parts </h1>
     <h2> Order results </h2>
+    <h3> Discount offered: <br /></br>
+
+         Fewer than 10 tires purchased- No discount </br>
+         10 - 49 tires purchased      - 5% discount </br>
+         50 - 99 tires purchased      - 10% Discount </br>
+         100 or more tires purchased  - 15% discount </br>
+
+    </h3>
     <?php
 
       $tireqty = $_POST['tireqty'];
       $oilqty = $_POST['oilqty'];
       $sparkqty = $_POST['sparkqty'];
 
+      if($tireqty < 10){
+        $discount = 0;
+      }
+      else if($tireqty>9 && $tireqty<50){
+        $discount = 5;
+      }
+      else if($tireqty>49 && $tireqty<100){
+        $discount = 10;
+      }
+      else if($tireqty>99){
+        $discount = 15;
+      }
+
       echo "<div class = \"listOfItems\">
-        $tireqty      Tire(s) <br />
-        $oilqty       Oil bottle(s) <br />
-        $sparkqty     Spark plug(s) <br />
+        $tireqty     <span class=\"iDiv\"> Tire(s) </span><br />
+        $oilqty      <span class=\"iDiv\"> Oil bottle(s) </span><br />
+        $sparkqty    <span class=\"iDiv\"> Spark plug(s) </span><br />
       </div>";
 
 
@@ -51,7 +78,9 @@
 
       $taxrate = 0.10;
       $totalamount = $totalamount + $taxrate * $totalamount;
-      echo "Total including tax: &emsp;&emsp;$" . number_format($totalamount) . "</p>";
+      echo "Total including tax: &emsp;&emsp;$" . number_format($totalamount) . "<br />";
+      echo "Discount: &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;" . $discount. "%<br />";
+      echo "Total after discount: &emsp;$" . ($totalamount - ($discount/100 * $totalamount)) . "</p>";
     ?>
 
   </body>
